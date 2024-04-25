88092b289e54f13fbef299c32c9741a87ea70127
morteza-nouri <morteza1noori@gmail.com>
2024-04-25 14:53:17 +0330

    Enrich test, but still fails!

--- src/test/java/org/rebecalang/compiler/modelcompiler/MailboxTest.java
+++ src/test/java/org/rebecalang/compiler/modelcompiler/MailboxTest.java
@@ -3,9 +3,11 @@ package org.rebecalang.compiler.modelcompiler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.rebecalang.compiler.CompilerConfig;
+import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.RebecaModel;
 import org.rebecalang.compiler.utils.CompilerExtension;
 import org.rebecalang.compiler.utils.CoreVersion;
 import org.rebecalang.compiler.utils.ExceptionContainer;
+import org.rebecalang.compiler.utils.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -29,7 +31,7 @@ public class MailboxTest {
         Set<CompilerExtension> extension = new HashSet<CompilerExtension>();
         extension.add(CompilerExtension.TIMED_REBECA);
 
-        compiler.compileRebecaFile(model, extension, CoreVersion.CORE_2_1);
+        Pair<RebecaModel, SymbolTable> result = compiler.compileRebecaFile(model, extension, CoreVersion.CORE_2_1);
 
         exceptionContainer.print(System.out);
         Assertions.assertTrue(exceptionContainer.exceptionsIsEmpty());
