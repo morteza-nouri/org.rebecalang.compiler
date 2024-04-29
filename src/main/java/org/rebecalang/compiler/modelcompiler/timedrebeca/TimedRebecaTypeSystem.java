3043918827aaf79dcd381716105056546a5468c7
morteza-nouri <morteza1noori@gmail.com>
2024-04-29 19:17:24 +0330

    Enrich TimedRebecaTypeSystem to support Mailbox

--- src/main/java/org/rebecalang/compiler/modelcompiler/timedrebeca/TimedRebecaTypeSystem.java
+++ src/main/java/org/rebecalang/compiler/modelcompiler/timedrebeca/TimedRebecaTypeSystem.java
@@ -1,28 +1,58 @@
 package org.rebecalang.compiler.modelcompiler.timedrebeca;
 
+import lombok.Getter;
 import org.rebecalang.compiler.modelcompiler.corerebeca.CoreRebecaTypeSystem;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.OrdinaryPrimitiveType;
+import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.Type;
+import org.rebecalang.compiler.modelcompiler.timedrebeca.objectmodel.MailboxDeclaration;
+import org.rebecalang.compiler.utils.CodeCompilationException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Qualifier("TIMED_REBECA")
 public class TimedRebecaTypeSystem extends CoreRebecaTypeSystem {
 
-	@Getter
-	public final static OrdinaryPrimitiveType TIMER_TYPE;
-	
-	static {
-		TIMER_TYPE = new OrdinaryPrimitiveType();
-		TIMER_TYPE.setName("Timer");
-	}
+    @Getter
+    public final static OrdinaryPrimitiveType TIMER_TYPE;
 
-	@Override
-	protected void initializeTypeSystem() {
-		super.initializeTypeSystem();
-		
-		addNewType(TIMER_TYPE);
-	}
+    @Getter
+    public final static OrdinaryPrimitiveType MAILBOX_TYPE;
+
+    protected Map<Type, MailboxDeclaration> mailboxClassesMetaData;
+
+    static {
+        TIMER_TYPE = new OrdinaryPrimitiveType();
+        TIMER_TYPE.setName("Timer");
+        MAILBOX_TYPE = new OrdinaryPrimitiveType();
+        MAILBOX_TYPE.setName("Mailbox");
+    }
+
+    @Override
+    protected void initializeTypeSystem() {
+        super.initializeTypeSystem();
+
+        mailboxClassesMetaData = new HashMap<>();
+
+        addNewType(TIMER_TYPE);
+        addNewType(MAILBOX_TYPE);
+    }
+
+    public Type addMailboxClassType(MailboxDeclaration mbd) {
+        OrdinaryPrimitiveType type = new OrdinaryPrimitiveType();
+        type.setName(mbd.getName());
+        type.setLineNumber(mbd.getLineNumber());
+        type.setCharacter(mbd.getCharacter());
+        addNewType(type);
+        mailboxClassesMetaData.put(type, mbd);
+        try {
+            addTypeCompatibility(getType(mbd.getName()), MAILBOX_TYPE);
+        } catch (CodeCompilationException e) {
+            e.printStackTrace();
+        }
+        return type;
+    }
 }
---
+++ src/main/java/org/rebecalang/compiler/modelcompiler/timedrebeca/TimedRebecaTypeSystemInitializer.java
@@ -0,0 +1,38 @@
+package org.rebecalang.compiler.modelcompiler.timedrebeca;
+
+import org.rebecalang.compiler.modelcompiler.abstractrebeca.AbstractTypeSystem;
+import org.rebecalang.compiler.modelcompiler.abstractrebeca.TypeSystemInitializer;
+import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.RebecaModel;
+import org.rebecalang.compiler.modelcompiler.timedrebeca.objectmodel.MailboxDeclaration;
+import org.rebecalang.compiler.modelcompiler.timedrebeca.objectmodel.TimedRebecaCode;
+import org.rebecalang.compiler.utils.CodeCompilationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TimedRebecaTypeSystemInitializer extends TypeSystemInitializer {
+
+    @Autowired
+    public TimedRebecaTypeSystemInitializer(AbstractTypeSystem typeSystem) {
+        super(typeSystem);
+    }
+
+    protected void fillTypeSystem(RebecaModel rebecaModel) {
+        super.fillTypeSystem(rebecaModel);
+        addMailboxClassesToTypeSystem((TimedRebecaCode) rebecaModel.getRebecaCode());
+    }
+
+    private void addMailboxClassesToTypeSystem(TimedRebecaCode rebecaCode) {
+        for (MailboxDeclaration mailboxDeclaration : rebecaCode.getMailboxDeclaration()) {
+            if (typeSystem.hasType(mailboxDeclaration.getName())) {
+                CodeCompilationException rce = new CodeCompilationException(
+                        "Multiple Definition of " + mailboxDeclaration.getName(),
+                        mailboxDeclaration.getLineNumber(),
+                        mailboxDeclaration.getCharacter()
+                );
+                exceptionContainer.addException(rce);
+            } else {
+                ((TimedRebecaTypeSystem) typeSystem).addMailboxClassType(mailboxDeclaration);
+            }
+        }
+    }
+
+}
